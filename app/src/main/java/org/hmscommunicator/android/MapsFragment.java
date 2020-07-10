28601ff86dbcf473b3ea135eca4559914735f5ac
Al Hua <sehua2000@yahoo.com>
2020-07-09 20:42:44 -0700

    Add initial project file

---
+++ app/src/main/java/org/hmscommunicator/android/MapsFragment.java
@@ -0,0 +1,64 @@
+package org.hmscommunicator.android;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.huawei.hms.maps.CameraUpdateFactory;
+import com.huawei.hms.maps.HuaweiMap;
+import com.huawei.hms.maps.OnMapReadyCallback;
+import com.huawei.hms.maps.SupportMapFragment;
+import com.huawei.hms.maps.model.LatLng;
+import com.huawei.hms.maps.model.MarkerOptions;
+
+public class MapsFragment extends Fragment {
+
+    private static final String TAG = "MapsFragment";
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(HuaweiMap huaweiMap) {
+//            LatLng sydney = new LatLng(-34, 151);
+//            huaweiMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//            huaweiMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            Log.d(TAG, "onMapReady: ");
+//            hMap = map;
+            huaweiMap.getUiSettings().setMyLocationButtonEnabled(true);
+            huaweiMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.804897,-122.477195), 10));
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
